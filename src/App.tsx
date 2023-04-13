c083be25fd6a0a69bb2e5e09c30176471f462871
Depetrol <lslrobert@outlook.com>
2023-04-12 23:40:01 -0700

    Update: Scaffold UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const ConnectMetamask = () => {
+    console.log(`Button 1 clicked with value: ${input1}`);
+  };
+
+  const TransferButton = () => {
+    console.log(`Button 2 clicked with value: ${input2}`);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ConnectMetamask}>Connect MetaMask</button>
+      <div>
+        <input
+          type="text"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+          placeholder="Enter Value in ETH"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+          placeholder="Enter Receiver Address"
+        />
+      </div>{' '}
+      <button onClick={TransferButton}>Tranasfer</button>
     </div>
   );
 }
